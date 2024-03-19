46386c3229ee009abb4caca89a44993ca950917d
Lucas Bortolo <lucasbortolo3@gmail.com>
2024-03-19 17:10:51 -0300

    initial commit

---
+++ src/test/java/br/com/hackaton/company/employeeregister/EmployeeRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hackaton.company.employeeregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
